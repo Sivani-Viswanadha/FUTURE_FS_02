1c146c65938655d9d4de3299a9d78b0e2a60a002
Sivani-Viswanadha <sivaniviswanadha@gmail.com>
2026-05-29 16:40:56 +0530

    deployment

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,6 +1,6 @@
 // Update API_BASE to point to your backend.
 window.CRM_CONFIG = {
-  API_BASE: " https://future-fs-02-2-t6ls.onrender.com",
+  API_BASE: " https://future-fs-02-2-t6ls.onrender.com/api",
   TOKEN_KEY: "crm_token",
   THEME_KEY: "crm_theme",
 };
